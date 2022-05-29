15c2e973fb3e8065430311c114cfb1bc2b3ce45c
Aditya <adityasingh220598@gmail.com>
2022-05-29 13:12:27 +0530

    Introducing links

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,22 @@
 // The page will be served for the url /news
 
+import Link from "next/link";
+import { Fragment } from "react";
+
 const NewsPage = () => {
-  return <h1>News page</h1>;
+  return (
+    <Fragment>
+      <h1>Detail Page</h1>
+      <ul>
+        <li>
+          <Link href={"/news/nextjs-is-a-great-framework"}>
+            Next JS is a great framework
+          </Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
