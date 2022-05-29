108dcbe3062c14d4f7747713e7b475880e4ca4d5
Aditya <adityasingh220598@gmail.com>
2022-05-29 11:36:13 +0530

    Nested Routing

---
+++ pages/news/something-important.js
@@ -0,0 +1,15 @@
+const SomethingImportant = () => {
+  return (
+    <div>
+      <h1>Something Important</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        doloremque, dolorum, quia quibusdam quidem quisquam doloribus
+        consequuntur, voluptatem ipsum quaerat doloremque nesciunt, quos
+        voluptatum quod.
+      </p>
+    </div>
+  );
+};
+
+export default SomethingImportant;
